6e9b5c0a51cdf89f365732ff296c9e593ce84b01
kani <kanishkas@usf.edu>
2025-01-26 15:54:32 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+products.push("Webcam"); // Add a new product
+products.pop(); // Remove the last product
+console.log("Updated products:", products);
